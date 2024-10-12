779a3398fcba05604030443187028b1f809314a9
Reinaldo <rms_master@hotmail.com>
2024-10-12 02:32:08 -0300

    registernewsurvivor.js

--- commands/utility/registernewsurvivor.js
+++ commands/utility/registernewsurvivor.js
@@ -1,5 +1,7 @@
 const {SlashCommandBuilder, PermissionsBitField} = require('discord.js');
 const {exec} = require('child_process');
+const crypto = require('crypto');
+
 
 const survivorRoleId = '1221553397522632775';
 const newPlayerRoleId = '1291957927535710218';
@@ -30,7 +32,7 @@ async function rconMessage(message) {
     const util = require('util');
     const exec = util.promisify(require('child_process').exec);
     try {
-        const {stdout, stderr} = await exec("/games/rcon/rcon --config /games/Zomboid/Server/rcon.yaml ${message}");
+        const {stdout, stderr} = await exec("/games/rcon/rcon --config /games/Zomboid/Server/rcon.yaml '" + message + "'");
         return {stdout, stderr};
     } catch (err) {
         console.error(err);
@@ -38,9 +40,9 @@ async function rconMessage(message) {
 }
 
 async function generateRandomPassword() {
-    crypto = require('crypto');
-    crypto.randomBytes(3, function (err, buffer) {
-        console.log(parseInt(buffer.toString('hex'), 16).toString().substring(0, 6));
+    const util = require('util');
+    return util.promisify(crypto.randomBytes)(3).then(buffer => {
+        return parseInt(buffer.toString('hex'), 16).toString().substring(0, 6);
     });
 }
 
@@ -52,7 +54,7 @@ module.exports = {
         .setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator),
     async execute(interaction) {
         if (!interaction.member.permissionsIn(interaction.channel).has(PermissionsBitField.Flags.Administrator)) {
-            await interaction.reply("You must be an administrator to perform this action.");
+            await interaction.reply("Você precisa ser administrador para executar esse comando.");
             return;
         }
         const channel = interaction.channel;
@@ -73,36 +75,43 @@ module.exports = {
         await respondInteraction(interaction, 'verificando se existe ' + nicknameRequested + ' no servidor.');
 
         if (interaction.guild.members.cache.find(member => member.nickname === nicknameRequested)) {
-            await respondInteraction(interaction, "usuário" + nicknameRequested + " já existe no servidor", false);
+            await respondInteraction(interaction, "usuário " + nicknameRequested + " já existe no servidor do discord, por favor, abra um novo ticket com um novo nick.", false);
             return;
         }
 
-        // await memberRequestingCreation.setNickname(nicknameRequested);
-        // await respondInteraction(interaction, 'apelido definido de ' + memberRequestingCreation.username + ' para ' + nicknameRequested + ' no servidor.');
+        const password = await generateRandomPassword();
 
-        const password = generateRandomPassword();
+        const result = await rconMessage(`adduser \"${nicknameRequested}\" ${password}`);
 
-        const result = rconMessage(`adduser ${nicknameRequested} ${password}`);
-        if(result.stdout.includes("created")) {
-            await respondInteraction(interaction, "Usuário " + nicknameRequested + " criado no servidor");
-        }
-
-        if(result.stdout.includes("already exists")) {
+        if (result.stdout?.includes("already exists")) {
             await respondInteraction(interaction, "Usuário " + nicknameRequested + " já existe, por favor, abra um novo ticket com um novo nick.", false);
+            return;
         }
 
-        if(result.stdout.includes("Invalid username")) {
+        if (result.stdout?.includes("Invalid username")) {
             await respondInteraction(interaction, "Usuário " + nicknameRequested + " é inválido, por favor, abra um novo ticket com um novo nick.", false);
+            return;
         }
 
-        if(result.stderr?.length > 0) {
+        if (result.stderr?.length > 0) {
             await respondInteraction(interaction, "Um erro desconhecido aconteceu");
             console.error(result.stderr);
             return;
         }
 
+        if (result.stdout?.includes("success") || result.stdout?.includes("created")) {
+            await respondInteraction(interaction, "Usuário " + nicknameRequested + " criado no servidor");
+            await memberRequestingCreation.setNickname(nicknameRequested);
+            await respondInteraction(interaction, 'apelido alterado de ' + memberRequestingCreation.username + ' para ' + nicknameRequested + ' no servidor.');
 
-        await respondInteraction(interaction, 'Usuario enviado via DM, mas caso não chegue, aqui está!\n\nSeu usuário: ' + nicknameRequested + '\nSenha: ' + password, false);
-        memberRequestingCreation.send('Seja bem vindo ao servidor!\n\nSeu usuário: ' + nicknameRequested + '\nSenha: ' + password);
+
+            await respondInteraction(interaction, 'Usuario enviado via DM, mas caso não chegue, aqui está!\n\nUsuário: ' + nicknameRequested + '\nSenha: ' + password, false);
+            try {
+                await memberRequestingCreation.send('Seja bem vindo ao servidor Saviors!\n\nUsuário: ' + nicknameRequested + '\nSenha: ' + password);
+            } catch (e) {
+                console.error(e);
+                await respondInteraction(interaction, 'Não foi possível enviar por DM, então, por favor anote sua senha pois você não terá acesso a este ticket depois.', false);
+            }
+        }
     },
 };
