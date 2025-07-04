14722a0cb8be318b6084505caeffe44db5f530ba
Reinaldo <rms_master@hotmail.com>
2025-07-03 22:21:33 -0300

    change do s7

--- utils/rcon.js
+++ utils/rcon.js
@@ -2,7 +2,7 @@ async function execute(message) {
     const util = require('util');
     const exec = util.promisify(require('child_process').exec);
     try {
-        const {stdout, stderr} = await exec("/games/rcon/rcon --config /games/Zomboid/Server/rcon.yaml '" + message + "'");
+        const {stdout, stderr} = await exec("/saviors-s7/rcon/rcon --config /saviors-s7/Zomboid/Server/rcon.yaml '" + message + "'");
         return {stdout, stderr};
     } catch (err) {
         return {stdout: null, stderr: err};
